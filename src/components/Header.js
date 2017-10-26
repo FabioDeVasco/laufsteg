6cc1e67338e7ad6996cdb16a144884970414e916
FabioDeVasco <de.vasconcelos.fabio@gmail.com>
2017-10-26 22:55:31 +0200

    Add <Navi>

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,16 +5,66 @@ import styled from 'styled-components';
 
 const Wrapper = styled.div`
 	display: flex;
+	flex-direction: row;
 	justify-content: center;
 	background-color: #606060
+
+`;
+
+const Navi = styled.div`
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
 
 
 `;
 
+const Opening = styled.div`
+	font-size: 10pt;
+	color: white;
+	margin-top: 5rem;
+	justify-content: center;
+`;
+
+const Links = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	justify-content: center;
+
+`;
+
+const Linksname = styled.div`
+font-size: 10pt;
+color: white;
+
+`;
+
+const Event = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	justify-content: center;
+
+`;
+
+const Eventname = styled.div`
+font-size: 10pt;
+color: #ED024B;
+
+`;
+
+const Eventdate = styled.div`
+font-size: 10pt;
+color: white;
+
+`;
+
+
 const Titel = styled.div`
 	font-size: 24pt;
 	color: white;
-
+	justify-content: center;
 
 `;
 
@@ -23,6 +73,24 @@ class Header extends Component {
 	render() {
 		return (
 			<Wrapper>
+				<Navi>
+					<Opening>
+						ÖFFNUNGSZEITEN MO.-SA. 10.00-18.00 UHR
+					</Opening>
+					<Links>
+						<Linksname>KONTAKT</Linksname>
+						<Linksname>ÜBER UNS</Linksname>
+						<Linksname>UNSERE MARKEN</Linksname>
+					</Links>
+					<Event>
+						<Eventname>
+							NIGHTSHOPING
+						</Eventname>
+						<Eventdate>
+							07.10.16
+						</Eventdate>
+					</Event>
+				</Navi>
 				<Titel> DAMENBEKLEIDUNG/ TASCHEN/ ACCESSOIRES </Titel>
 			</Wrapper>
 		);
