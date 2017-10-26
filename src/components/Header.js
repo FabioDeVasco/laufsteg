52dddcbf0ca9d8c73eba80de71dafbb73f61d56b
FabioDeVasco <de.vasconcelos.fabio@gmail.com>
2017-10-26 22:14:37 +0200

    Add <Titel> to <Header>

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,27 @@ import styled from 'styled-components';
 // import { Color } from '../utils';
 
 const Wrapper = styled.div`
-	font-size: 24pt;
-	color: #FFFFFF;
+	display: flex;
+	justify-content: center;
+	background-color: #606060
+
+
 `;
+
+const Titel = styled.div`
+	font-size: 24pt;
+	color: white;
+
+
+`;
+
 
 class Header extends Component {
 	render() {
 		return (
-			<Wrapper>Laufsteg</Wrapper>
+			<Wrapper>
+				<Titel> DAMENBEKLEIDUNG/ TASCHEN/ ACCESSOIRES </Titel>
+			</Wrapper>
 		);
 	}
 }
