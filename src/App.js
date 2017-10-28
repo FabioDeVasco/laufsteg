578a96a40ff884d607944a850663dfe27e596944
FabioDeVasco <de.vasconcelos.fabio@gmail.com>
2017-10-28 16:45:22 +0200

    Integrate Google Font

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { injectGlobal } from 'styled-components';
+
+import { Font, FONT_URL } from './utils';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,10 +13,25 @@ class App extends Component {
     return (
       <div className="App">
         <Header />
-      
+
       </div>
     );
   }
 }
+
+injectGlobal([
+  `
+  @font-face {
+    font-family: ${Font.PRIMARY};
+    font-family: ${Font.SECONDARY};
+    src: url(${FONT_URL});
+  }
+
+  body {
+    font-family: ${Font.PRIMARY};
+    margin: 0;
+  }
+`,
+]);
 
 export default App;
--- src/utils/font.js
+++ src/utils/font.js
@@ -1,1 +1,10 @@
+// font family
+const PRIMARY = "'Pacifico', cursive";
+const SECONDARY = "'Cinzel', serif";
 
+export const FONT_URL = 'https://fonts.googleapis.com/css?family=Pacifico|Roboto';
+
+export const Font = {
+  PRIMARY,
+  SECONDARY,
+};
