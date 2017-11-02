730704517f1e508291d0a4353d0df2370bda7896
FabioDeVasco <de.vasconcelos.fabio@gmail.com>
2017-11-02 23:49:55 +0100

    <NextEvent> with <EventText>,<EventPicture>, <EventTitel>

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from './components/Header';
+import NextEvent from './components/NextEvent';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-
+        <NextEvent />
       </div>
     );
   }
