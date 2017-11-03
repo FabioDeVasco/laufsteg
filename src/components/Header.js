9ff28da07bc04faa49a321a75afc1aa8ee3d829d
FabioDeVasco <de.vasconcelos.fabio@gmail.com>
2017-11-03 15:21:22 +0100

    <NextEvent> update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { font } from '../utils';
+
 // import { Color } from '../utils';
+
+
 
 const Wrapper = styled.div`
 	display: flex;
@@ -9,24 +11,22 @@ const Wrapper = styled.div`
 	justify-content: center;
 	background-color: #606060
 
-
 `;
 
 const Navi = styled.div`
 	display: flex;
 	justify-content: center;
 	flex-direction: column;
-	font-family: serif;
-
-
+	text-transform: uppercase;
+	letter-spacing: 2px;
+	flex: 1;
 
 `;
 
 const Opening = styled.div`
 	font-size: 10pt;
 	color: white;
-	margin-top: 10rem;
-
+	margin-top: 1rem;
 
 `;
 
@@ -34,15 +34,20 @@ const Links = styled.div`
 	display: flex;
 	flex-direction: column;
 	flex-wrap: wrap;
+	align-items: center;
 	justify-content: center;
-	margin-top: 5rem;
-	margin-bottom: 5rem;
+	margin-top: 7rem;
+	margin-bottom: 7rem;
+
 `;
 
 const Linksname = styled.div`
-font-size: 10pt;
-color: white;
-margin-bottom: 2rem;
+	font-size: 10pt;
+	color: white;
+	margin-bottom: 1rem;
+	margin-top: 1em;
+	padding-left: 2rem;
+	padding-right: 2rem;
 
 `;
 
@@ -52,85 +57,97 @@ const Event = styled.div`
 	flex-wrap: wrap;
 	justify-content: center;
 	margin-bottom: 5rem;
-
 `;
 
 const Eventname = styled.div`
-font-size: 10pt;
-color: #ED024B;
-
-
+	font-size: 10pt;
+	color: #ED024B;
 `;
 
 const Eventdate = styled.div`
-font-size: 10pt;
-color: white;
-
+	font-size: 10pt;
+	color: white;
 `;
 
-
-const Welcome= styled.div`
-display: flex;
-flex-direction: column;
-
+const Welcome = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex:6;
+	justify-content: center;
+	width: 90%;
+	text-transform: uppercase;
 `;
 
 const Titel = styled.div`
-	font-size: 24pt;
+	font-size: 20pt;
 	color: white;
-	justify-content: center;
-	width: 95%;
-	font-family: serif;
-	margin-top: 1rem;
-
+	padding-top: 0.5rem;
+	padding-bottom: 1rem;
+	letter-spacing: 10px;
 `;
-
 
 const Wallpaper = styled.div`
 background-image: url(${require('../assets/images/Wallpaperpic.png')});
-
-
+background-position: 100%;
+background-size: 100%, 100%;
+background-repeat: no-repeat;
+height: 90%;
 `;
 
 const Brand = styled.div`
 border-color: white;
-font-family: serif;
-margin-top: 10rem;
+border: 4px solid ;
+border-radius: 10px;
+font-size: 48pt;
+letter-spacing: 4px;
+color: white;
+margin-top: 2rem;
+margin-bottom: 30rem;
+margin-left: 20rem;
+margin-right: 20rem;
+padding-left: 1rem;
+padding-right: 1rem;
+padding-top: 1rem;
+padding-bottom: 1rem;
 `;
 
 class Header extends Component {
-	render() {
-		return (
-			<Wrapper>
-				<Navi>
-					<Opening>
-						ÖFFNUNGSZEITEN MO.-SA. 10.00-18.00 UHR
-					</Opening>
-					<Links>
-						<Linksname>KONTAKT</Linksname>
-						<Linksname>ÜBER UNS</Linksname>
-						<Linksname>UNSERE MARKEN</Linksname>
-					</Links>
-					<Event>
-						<Eventname>
-							NIGHTSHOPING
-						</Eventname>
-						<Eventdate>
-							07.10.16
-						</Eventdate>
-					</Event>
-				</Navi>
-				<Welcome>
-					<Titel> DAMENBEKLEIDUNG/ TASCHEN/ ACCESSOIRES </Titel>
-					<Wallpaper>
-						<Brand>
-							LAUFSTEG
-						</Brand>
-					</Wallpaper>
-				</Welcome>
-			</Wrapper>
-		);
-	}
+  render() {
+    return (
+	      <Wrapper>
+	        <Navi>
+	          <Opening>
+	            <div>Öffnungszeiten</div>
+	            {' '}
+	            <div>Mo.-Sa.</div>
+	            {' '}
+	            <div>10.00-18.00 Uhr</div>
+	          </Opening>
+	          <Links>
+	            <Linksname>Kontakt</Linksname>
+	            <Linksname>Über uns</Linksname>
+	            <Linksname>Unsere Marken</Linksname>
+	          </Links>
+	          <Event>
+	            <Eventname>
+	              Nightshopping
+	            </Eventname>
+	            <Eventdate>
+	              07.10.16
+	            </Eventdate>
+	          </Event>
+	        </Navi>
+	        <Welcome>
+	          <Titel> Damenbekleidung/ Taschen/ Accessoires </Titel>
+	          <Wallpaper>
+	            <Brand>
+	              Laufsteg
+	            </Brand>
+	          </Wallpaper>
+	        </Welcome>
+	      </Wrapper>
+    );
+  }
 }
 
 export default Header;
---
+++ src/components/NextEvent.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+
+
+
+
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+
+
+  `;
+
+const TitelContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  `;
+
+  const Titel = styled.div`
+  	font-size: 30pt;
+  	color: black;
+  	margin-top: 5rem;
+    margin-bottom: 1.5rem;
+    margin-right: 2rem;
+    letter-spacing: 15px;
+    text-align: right;
+    text-transform: uppercase;
+
+
+  `;
+
+  const TitelUnderline = styled.div`
+    border-bottom: 3px solid;
+    border-color: #ED024B;
+    margin-left: 40rem;
+    margin-top: -1.5rem;
+
+    `;
+
+
+
+  const EventBody = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  `;
+
+  const EventPicture = styled.div`
+  flex:1;
+  background-image: url(${require('../assets/images/Eventpic.jpg')});
+  background-position: left;
+  background-size: contain;
+  background-repeat: no-repeat;
+  padding-bottom: 20rem;
+  padding-top: 20rem;
+  margin-left: -2rem;
+
+
+  `;
+
+  const EventText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  flex:1.5;
+  letter-spacing: 8px;
+  font-size: 16pt;
+  text-align: justify;
+  margin-right: 2rem;
+  margin-top: -8rem;
+
+  line-height: 1.8rem;
+  `;
+
+
+
+  class NextEvent extends Component {
+    render() {
+      return (
+  	      <Wrapper>
+            <TitelContainer>
+              <Titel>
+                Fashion meets Party
+              </Titel>
+              <TitelUnderline>{' '}</TitelUnderline>
+            </TitelContainer>
+            <EventBody>
+              <EventPicture>
+
+              </EventPicture>
+              <EventText>
+                <div>Zwei mal im Jahr veranstalten wir aus dem Laufsteg eine kleine Feier mit Drinks und Dips um euch die neue Kollektionen vorzustellen.</div>
+                 {' '}
+                <div>Wir möchten euch ganz herzlich einladen um an unserem Event teilzunehmen. Wir freuen uns auf euer Erscheinen und einen tollen Abend mit ganz viel Spaß</div>
+              </EventText>
+            </EventBody>
+          </Wrapper>
+       );
+     }
+   }
+
+   export default NextEvent;
