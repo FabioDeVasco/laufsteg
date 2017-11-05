d8ca3998312d35054c81d90d44c996e0dfbaeab6
FabioDeVasco <de.vasconcelos.fabio@gmail.com>
2017-11-05 19:53:20 +0100

    Overwork Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,9 +8,7 @@ const Wrapper = styled.div`
 	display: flex;
 	flex-direction: column;
 	justify-content: center;
-  background-color: #606060;
-  color: white;
-
+  background-color: #DDDDDD;
   `;
 
 
@@ -24,13 +22,13 @@ const Wrapper = styled.div`
 
     const IntroSpacer = styled.div`
       display: flex;
-      flex-direction: row;
-      justify-content: center;
-      border-bottom: 3px solid;
-      border-color: red;
-      margin-top: 1rem;
+      border-top: 1px solid;
+      border-color: black;
+      margin-top: 3.5rem;
+      margin-left: 1rem;
+      margin-right:1rem;
       padding-top: 2rem;
-      padding-bottom: 2rem;
+      width: 28rem;
 
 
       `;
@@ -39,42 +37,41 @@ const Wrapper = styled.div`
       display: flex;
       flex-direction: row;
       justify-content: center;
+      margin-top: 1rem;
       padding-top: 2rem;
-      font-size: 20pt;
-      color: white;
+      font-size: 18pt;
+      color: black;
       text-transform: uppercase;
       margin-bottom: 1rem;
 
       `;
 
 
-      const Impressumbox = styled.div`
-          display: flex;
-          flex-direction: row;
-          justify-content: center;
-          margin-top: 3rem;
-          margin-bottom: 3rem;
+    const ImpressumBox = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        margin-top: 2rem;
+        margin-bottom: 2rem;
 
-
-          `;
+      `;
 
     const ImpressumPart = styled.div`
         display: flex;
         flex-direction: column;
         justify-content: center;
-        margin-left: 10rem;
-        margin-right: 10rem;
-
+        margin-left: 2rem;
+        margin-right: 2rem;
 
         `;
 
     const ImpressumTitel = styled.div`
         display: flex;
-        flex-direction: column;
-        justify-content: center;
+        flex-direction: row;
         color: #ED024B;
         text-transform: uppercase;
-
+        text-align: left;
+        margin-bottom: 1rem;
 
         `;
 
@@ -82,22 +79,53 @@ const Wrapper = styled.div`
         display: flex;
         flex-direction: column;
         justify-content: center;
-        flex:1;
-        letter-spacing: 3px;
-        font-size: 16pt;
+        font-size: 12pt;
         text-align: justify;
-        margin-left: 2rem;
         line-height: 1.8rem;
-        color: white;
-        background-color: red;
-        padding-left: 10rem;
-        padding-right: 10rem;
-        margin-left: -2rem;
-        margin-right: -2rem;
+        color: black;
 
 
         `;
 
+      const ImpressumIconBox = styled.div`
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+
+        `;
+
+      const ImpressumIconPart = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+
+        `;
+
+      const ImpressumIcon = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        color: black;
+        margin-left: 0.3rem;
+        margin-right: 0.3rem;
+        `;
+
+      const ImpressumIconArrow = styled.div`
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+        color: #ED024B;
+        margin-top: 2rem;
+
+        `;
+        const ImpressumDesignBox = styled.div`
+          display: flex;
+          flex-direction: column;
+          justify-content:flex-end;
+          font-style: italic;
+          color: black;
+          text-align: right;
+          `;
 
 
   class Footer extends Component {
@@ -105,19 +133,19 @@ const Wrapper = styled.div`
       return (
   	      <Wrapper>
             <Intro>
-              <IntroSpacer></IntroSpacer>
+              <IntroSpacer>{' '}</IntroSpacer>
               <IntroTitel>Impressum Laufsteg</IntroTitel>
-              <IntroSpacer></IntroSpacer>
+              <IntroSpacer>{' '}</IntroSpacer>
             </Intro>
-            <Impressumbox>
+            <ImpressumBox>
               <ImpressumPart>
                 <ImpressumTitel>
-                  Sitz
+                  Gesellschaft
                 </ImpressumTitel>
                 <ImpressumText>
                   <div>Sitz der Gesellschaft: Heilbronn</div>
                  {' '}
-                 <div>Geschäftsführende Gesellschafterin: Fatma Öztoprak</div>
+                  <div>Geschäftsführende Gesellschafterin: Fatma Öztoprak</div>
                 </ImpressumText>
               </ImpressumPart>
               <ImpressumPart>
@@ -125,18 +153,50 @@ const Wrapper = styled.div`
                   Kontakt
                 </ImpressumTitel>
                 <ImpressumText>
-                  hier steht Kontakt
+                  <div>Anschrift:  Biedermanngasse 4, 74072 Heilbronn</div>
+                  {' '}
+                  <div>Telefon:    07131 203 18 81</div>
+                  {' '}
+                  <div>E-Mail:   laufsteg.heilbronn@t-online.de</div>
                 </ImpressumText>
               </ImpressumPart>
               <ImpressumPart>
                 <ImpressumTitel>
-                  Verantwortung
+                  Handelsregister
                 </ImpressumTitel>
                 <ImpressumText>
-                  hier steht verantwortung
+                  <div>Handelsregister: AG Stuttgart HRB 1234</div>
+                  {' '}
+                  <div>Umsatzsteuer-Ident.-Nr.: DE XXXXXXXX</div>
+                  {' '}
+                  <div>Inhaltlich Verantwortlicher: Fatma Öztoprak</div>
                 </ImpressumText>
               </ImpressumPart>
-            </Impressumbox>
+            </ImpressumBox>
+            <ImpressumIconBox>
+              <ImpressumIconPart>
+                <ImpressumIcon>
+                  <i className="fa fa-facebook-square fa-1x" aria-hidden="true"></i>
+                </ImpressumIcon>
+                <ImpressumIcon>
+                  <i class="fa fa-instagram fa-1x" aria-hidden="true"></i>
+                </ImpressumIcon>
+                <ImpressumIcon>
+                  <i class="fa fa-envelope-o fa-1x" aria-hidden="true"></i>
+                </ImpressumIcon>
+                <ImpressumIcon>
+                  <i class="fa fa-phone fa-1x" aria-hidden="true"></i>
+                </ImpressumIcon>
+              </ImpressumIconPart>
+              <ImpressumIconPart>
+                <ImpressumIconArrow>
+                  <i class="fa fa-chevron-up fa-2x" aria-hidden="true"></i>
+                </ImpressumIconArrow>
+              </ImpressumIconPart>
+            </ImpressumIconBox>
+            <ImpressumDesignBox>
+              Design @ Zackteck
+            </ImpressumDesignBox>
           </Wrapper>
       );
     }
