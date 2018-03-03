6f9eebe0d45211a6321cff2b327703a8be00582e
FabioDeVasco <de.vasconcelos.fabio@gmail.com>
2018-03-03 11:45:22 +0100

    add google map api

--- server/client/src/components/contactComponents/Location.js
+++ server/client/src/components/contactComponents/Location.js
@@ -9,10 +9,11 @@ import {
 
 const Location = compose(
   withProps({
-    googleMapURL: 'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBYg07orlfcDlnBEMWb0BgEHZB7yoqYsRQ&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `75%` }} />,
     mapElement: <div style={{ height: `100%` }} />,
+
   }),
   withScriptjs,
   withGoogleMap,
