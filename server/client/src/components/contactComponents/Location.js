8d13ae6d79be11f6bc1f7ce3eda9b3d0df761a82
FabioDeVasco <de.vasconcelos.fabio@gmail.com>
2018-03-03 11:57:22 +0100

    new maps api

--- server/client/src/components/contactComponents/Location.js
+++ server/client/src/components/contactComponents/Location.js
@@ -9,7 +9,7 @@ import {
 
 const Location = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBYg07orlfcDlnBEMWb0BgEHZB7yoqYsRQ&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyAJWovhIO3whVLEzJAURh8AdRHD_eOLguk&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `75%` }} />,
     mapElement: <div style={{ height: `100%` }} />,
